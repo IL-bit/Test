5eac7e8cdab2c31f219265a09127b9cf47fb0a7d
Ilia Vlasov <vila6365@gmail.com>
2024-09-05 01:28:03 +0300

    Add redux

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import Table from './Table.js';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="row">
+        <div className="col-12">
+          <div className="header_Info">
+            <div>companySigDate</div>
+            <div>companySignatureName</div>
+            <div>documentName</div>
+            <div>documentStatus</div>
+            <div>documentType</div>
+            <div>employeeNumber</div>
+            <div>employeeSigDate</div>
+            <div>employeeSignatureName</div>
+          </div>
+        </div>
+        <div className="col-3">
+          <div className="header_buttons">
+            <button>Обновить</button>
+            <button>Создать</button>
+            <button>Выйти</button>
+          </div>
+
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-12">
+          <Table/>
+
+        </div>
+        <div className="col-3">
+          <div className="table_buttons">
+            <button>Изменить</button>
+            <button>Отправить</button>
+            <button>Удалить</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
